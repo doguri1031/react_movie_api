67d50ab4168315798e44ec24c10b5afbf4b68963
lee <doguri1031@gmail.com>
2020-07-06 22:19:37 +0900

    react movie project

---
+++ src/Components/Router.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { 
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch 
+} from "react-router-dom";
+import Home from "Components/Routes/Home";
+import TV from "Components/Routes/TV";
+import Header from "Components/Header";
+import Search from "Components/Routes/Search";
+import Detail from "Components/Routes/Detail";
+
+export default () => (
+    <Router>
+        <>
+        <Header />
+        <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/tv" component={TV}/>
+            <Route path="/tv/popular" render={() => <h1>Popular</h1>}/>
+            <Route path="/search" component={Search}/>
+            <Route path="/movie/:id" component={Detail} />
+            <Route path="/show/:id" component={Detail} />
+            <Redirect from="*" to="/" />
+        </Switch>
+        </>
+    </Router>
+); 
